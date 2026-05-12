09119c48425628a8a504627be76fb9f02b3afdc4
Suyash Gupta <suyashgupta34604@gmail.com>
2026-05-12 15:00:56 +0530

    comments added to main.jsx

--- frontend/instaClone/src/main.jsx
+++ frontend/instaClone/src/main.jsx
@@ -1,14 +1,32 @@
+// Importing StrictMode for highlighting potential React issues
 import { StrictMode } from 'react'
+
+// Importing createRoot for rendering React app
 import { createRoot } from 'react-dom/client'
+
+// Global CSS file
 import './index.css'
+
+// Main App Component
 import App from './App.jsx'
+
+// React Router for routing support
 import { BrowserRouter } from 'react-router-dom'
 
+// Rendering React Application
 createRoot(document.getElementById('root')).render(
-<BrowserRouter>
- <StrictMode>
-    <App />
-  </StrictMode>,
-</BrowserRouter>
+
+  // BrowserRouter enables routing in the app
+  <BrowserRouter>
+
+    {/* StrictMode helps in identifying problems during development */}
+    <StrictMode>
+
+      {/* Main Application Component */}
+      <App />
+
+    </StrictMode>
+
+  </BrowserRouter>
 
 )
